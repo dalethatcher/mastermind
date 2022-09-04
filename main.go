8f67e69118a4b9e7c60863e5a12a8a5d67e78e3b
Dale Thatcher <dale.thatcher@gmail.com>
2022-09-04 11:37:12 +0100

    Code generation function.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 type Score struct {
@@ -42,5 +43,25 @@ func CalculateScore(code []int, guess []int) Score {
 	return result
 }
 
+func IndexToCode(numberOfHoles int, numberOfColours int, index int) []int {
+	if index > int(math.Pow(float64(numberOfColours), float64(numberOfHoles)))-1 {
+		panic(fmt.Sprint("index ", index, " is larger than the number of combinations for ", numberOfHoles,
+			" holes and ", numberOfColours, " colours!"))
+	}
+
+	result := make([]int, numberOfHoles)
+	for i := numberOfHoles - 1; i >= 0 && index > 0; i-- {
+		colour := index % numberOfColours
+
+		result[i] = colour
+		index = index / numberOfColours
+	}
+
+	return result
+}
+
 func main() {
+	for i := 0; i < 256; i++ {
+		fmt.Println(IndexToCode(4, 4, i))
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
-type TestScenario struct {
-	code        []int
-	guess       []int
-	expectation Score
-}
-
 func TestScoreCalculation(t *testing.T) {
+	type TestScenario struct {
+		code        []int
+		guess       []int
+		expectation Score
+	}
+
 	scenarios := []TestScenario{
 		{code: []int{1, 2, 3, 4}, guess: []int{1, 1, 1, 1}, expectation: Score{rightValueAndPosition: 1}},
 		{code: []int{1, 2, 2, 2}, guess: []int{3, 1, 3, 3}, expectation: Score{rightValueWrongPosition: 1}},
@@ -23,6 +24,30 @@ func TestScoreCalculation(t *testing.T) {
 
 		if score != scenario.expectation {
 			t.Error("Expected score", scenario.expectation, "but got", score, "for code", scenario.code, "and guess", scenario.guess)
+		}
+	}
+}
+
+func TestIndexToCode(t *testing.T) {
+	type TestScenario struct {
+		numberOfColours int
+		index           int
+		expectation     []int
+	}
+
+	scenarios := []TestScenario{
+		{numberOfColours: 4, index: 0, expectation: []int{0, 0, 0, 0}},
+		{numberOfColours: 4, index: 1, expectation: []int{0, 0, 0, 1}},
+		{numberOfColours: 4, index: 255, expectation: []int{3, 3, 3, 3}},
+		{numberOfColours: 10, index: 4, expectation: []int{0, 0, 0, 4}},
+	}
+
+	for _, scenario := range scenarios {
+		code := IndexToCode(len(scenario.expectation), scenario.numberOfColours, scenario.index)
+
+		if !reflect.DeepEqual(code, scenario.expectation) {
+			t.Error("Expected code", scenario.expectation, "but got", code, "for index",
+				scenario.index, "and", scenario.numberOfColours, "colours")
 		}
 	}
 }
